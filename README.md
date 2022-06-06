b10f0e9a365f16a13bc06605706044ca235e2022
Scott <67354101+scGetStuff@users.noreply.github.com>
2022-06-05 19:41:18 -0500

    basic functionality done

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 The Odin Project, Full Stack JavaScript Path, JavaScript, Organizing your JavaScript Code, Project: Tic Tac Toe
 
 
-main goal is to have as little global code as possible; use module and/or factory
+The main goal is to have as little global code as possible; use module and/or factory.
 
-single page, responsive
+Using Revealing Module pattern.
+The page is responsive, at least in the browser tools, for this project I don't care what happens on a phone
+No need for user name field, I use left and right mouse for X & O; not concerned with UI, the module code is what matters.
--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 'use strict';
 
+
 const domStuff = (function () {
 
-    // only want to select things once 
+    // cache queries
     const cells = document.querySelectorAll('.cell');
     const resetButton = document.getElementById('reset');
+    const message = document.getElementById('message');
+    const options = document.getElementsByName('opt');
 
     // event bindings
     cells.forEach(cell => {
@@ -12,7 +15,9 @@ const domStuff = (function () {
         cell.addEventListener('contextmenu', rightClick);
     });
     resetButton.addEventListener('click', resetClick);
-
+    options.forEach(cell => {
+        cell.addEventListener('change', optionChange);
+    });
 
     function leftClick(e) {
         if (e.button !== 0)
@@ -24,19 +29,110 @@ const domStuff = (function () {
     function rightClick(e) {
         if (e.button !== 2)
             return;
+        if (!displayController.isSoloPlay())
+            return;
         e.preventDefault();
         displayController.setCell(e.target, 'O');
     }
 
     function resetClick(e) {
-        gameBoard.clear();
-        displayController.renderBoard();
+        displayController.reset();
+    }
+
+    function optionChange(e) {
+        displayController.changeOption(e.target);
+    }
+
+    function getGameType() {
+        for (let i = 0; i < options.length; i++) {
+            if (options[i].checked)
+                return options[i].value;
+        }
+        return null;
     }
 
     return {
-        cells
+        cells,
+        message,
+        getGameType
     }
 })();
+
+
+const displayController = (function () {
+
+    // read default from screen on load
+    let gameType = domStuff.getGameType();
+    let isGameOver = false;
+
+    function renderBoard() {
+        domStuff.cells.forEach(cell => {
+            cell.innerText = gameBoard.getCell(cell.dataset.cellnum);
+        });
+    }
+
+    function setMessage(msg) {
+        // TODO: there has to be a better way of doing this
+        // prevent empty p tag from making stuff shift on screen
+        if (!msg) {
+            domStuff.message.innerHTML = '&nbsp;';
+            return;
+        }
+        domStuff.message.innerText = msg;
+    }
+
+    function reset() {
+        isGameOver = false
+        gameBoard.clear();
+        setMessage('');
+        renderBoard();
+    }
+
+    function setCell(cell, value) {
+        // prevent overwrite cell
+        if (cell.innerText)
+            return;
+
+        // game over, make them reset
+        if (isGameOver) {
+            alert('Reset to play again');
+            return;
+        }
+
+        cell.innerText = value;
+        gameBoard.setCell(cell.dataset.cellnum, value);
+        // TODO: more stupid looking code, maybe i will fix it, maybe i will not care and move on to better backend stuff
+        if (gameBoard.hasWinner()) {
+            isGameOver = true;
+            setMessage(`${value} Wins!`);
+            return;
+        }
+        if (gameBoard.isTie()) {
+            isGameOver = true;
+            setMessage(`Tie`);
+            return;
+        }
+    }
+
+    function changeOption(opt) {
+        gameType = opt.value;
+        reset();
+    }
+
+    function isSoloPlay() {
+        return (gameType === 'self');
+    }
+
+    return {
+        renderBoard,
+        setCell,
+        setMessage,
+        reset,
+        changeOption,
+        isSoloPlay
+    }
+})();
+
 
 const gameBoard = (function () {
     const data = Array(9).fill(null);
@@ -58,30 +154,71 @@ const gameBoard = (function () {
         return data[cellnum - 1];
     }
 
+    // TODO: could i have made this any worse?
+    // copy paste looking crap, there is probably a better way 
+    // should have an api to translate grid cordinate to array index, 
+    // but all i need is the row/col check, so first pass half ass it
+    function hasWinner() {
+        let isSomethingFull = false;
+
+        isSomethingFull |= isRowFull(1) || isRowFull(2) || isRowFull(3);
+        isSomethingFull |= isColFull(1) || isColFull(2) || isColFull(3);
+        isSomethingFull |= isDiagFull();
+        return isSomethingFull;
+    }
+
+    function isTie() {
+        for (let i = 0; i < data.length; i++)
+            if (data[i] === null)
+                return false;
+        return true;
+    }
+
+    function isRowFull(row) {
+        const index = (row - 1) * 3;
+
+        if (data[index] == null)
+            return false;
+        return ((data[index] === data[index + 1]) && (data[index] === data[index + 2]));
+    }
+
+    function isColFull(col) {
+        const index = (col - 1);
+
+        if (data[index] == null)
+            return false;
+        return ((data[index] === data[index + 3]) && (data[index] === data[index + 6]));
+    }
+
+    // so fucking bad
+    function isDiagFull() {
+        if (data[0] != null)
+            if ((data[0] === data[4]) && (data[0] === data[8]))
+                return true;
+        if (data[2] != null)
+            if ((data[2] === data[4]) && (data[2] === data[6]))
+                return true;
+        return false;
+    }
+
     return {
-        dumpData,
         clear,
         setCell,
-        getCell
+        getCell,
+        hasWinner,
+        isTie
     }
 })();
 
-const displayController = (function () {
+// the project indicats that this should be a thing
+// i don't see it, i probalby have too much shit in the other modules
+const game = (function () {
 
-    function renderBoard() {
-        domStuff.cells.forEach(cell => {
-            cell.innerText = gameBoard.getCell(cell.dataset.cellnum);
-        });
-    }
+    function stuff() {
 
-    function setCell(cell, value) {
-        cell.innerText = value;
-        gameBoard.setCell(cell.dataset.cellnum, value);
     }
 
     return {
-        renderBoard,
-        setCell
+        stuff
     }
-
 })();
